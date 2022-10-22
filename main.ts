e96bd93608afe5b6054686998665daaa202da238
htbscHKBen <116170916+htbscHKBen@users.noreply.github.com>
2022-10-22 09:48:23 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+bluetooth.startMagnetometerService()
+bluetooth.startLEDService()
+let degrees = input.compassHeading()
+basic.showString("COMPASS")
+basic.forever(function () {
+    degrees = input.compassHeading()
+    if (degrees < 45 || degrees > 315) {
+        basic.showString("N")
+    } else if (degrees < 135) {
+        basic.showString("E")
+    } else if (degrees < 225) {
+        basic.showString("S")
+    } else {
+        basic.showString("W")
+    }
+})
